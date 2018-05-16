404f71524bb02f2561abfa4a62998e9bed1691e3
MuhammadBilal01 <i140232@nu.edu.pk>
2018-05-16 09:39:23 +0500

    AntProject

---
+++ SampleAntProject/src/SamplePackage/DateUtils.java
@@ -0,0 +1,19 @@
+package SamplePackage;
+
+import java.util.Date;
+
+public class DateUtils {
+
+	public static void main(String[] args) {
+
+		System.out.println(getLocalCurrentDate());
+		
+	}
+
+	private static Date getLocalCurrentDate() {
+	
+		return new Date();
+		
+	}
+
+}
